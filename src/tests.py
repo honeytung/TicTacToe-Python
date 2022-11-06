4a497103541f368e3854d1a9ec171b7e32b2e878
honeytung <honeytung@gmail.com>
2022-11-05 23:28:11 -0700

    Added more tests for get_winner(board) function

--- src/tests.py
+++ src/tests.py
@@ -4,13 +4,43 @@ import logic
 
 class TestLogic(unittest.TestCase):
 
+    def test_make_empty_board(self):
+        board = [[None, None, None],
+                 [None, None, None],
+                 [None, None, None]]
+        self.assertEqual(logic.make_empty_board(), board)
+
     def test_get_winner(self):
-        board = [
+        board1 = [
             ['X', None, 'O'],
             [None, 'X', None],
             [None, 'O', 'X']
         ]
-        self.assertEqual(logic.get_winner(board), 'X')
+        board2 = [
+            ['X', None, 'O'],
+            [None, 'O', None],
+            ['O', 'O', 'X']
+        ]
+        board3 = [
+            ['X', 'X', 'X'],
+            [None, 'X', None],
+            [None, 'O', 'O']
+        ]
+        board4 = [
+            ['X', None, 'O'],
+            [None, 'O', 'O'],
+            [None, 'O', 'O']
+        ]
+        board5 = [
+            ['X', None, 'O'],
+            [None, 'O', 'X'],
+            [None, 'O', 'O']
+        ]
+        self.assertEqual(logic.get_winner(board1), 'X')
+        self.assertEqual(logic.get_winner(board2), 'O')
+        self.assertEqual(logic.get_winner(board3), 'X')
+        self.assertEqual(logic.get_winner(board4), 'O')
+        self.assertEqual(logic.get_winner(board5), None)
 
     # TODO: Test all functions from logic.py
 
