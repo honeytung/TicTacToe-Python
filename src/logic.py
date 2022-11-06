d3bceee333fc61add9aede12600c0ee682fa3754
honeytung <honeytung@gmail.com>
2022-11-05 23:27:46 -0700

    Complete get_winner(board) function

--- src/logic.py
+++ src/logic.py
@@ -9,11 +9,34 @@ def make_empty_board():
         [None, None, None]
     ]
 
+
 def get_winner(board):
     """Determines the winner of the given board.
     Returns 'X', 'O', or None."""
-    return None # FIXME
+
+    # Check winner in rows
+    for row in board:
+        if row[0] == row[1] == row[2]:
+            return row[0]
+
+    col = 0
+
+    # Check winner in columns
+    while col < 3:
+        if board[0][col] == board[1][col] == board[2][col]:
+            return board[0][col]
+        col += 1
+
+    # Check winner in two diagonals
+    if board[0][0] == board[1][1] == board[2][2]:
+        return board[0][0]
+
+    if board[0][2] == board[1][1] == board[2][0]:
+        return board[0][2]
+
+    return None 
+
 
 def other_player(player):
     """Given the character for a player, returns the other player."""
-    return 'O' # FIXME
+    return 'O'  # FIXME
