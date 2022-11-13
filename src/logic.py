0d83dbaf241c5d135dd654cb6d10efedc9deceb3
honeytung <honeytung@gmail.com>
2022-11-12 20:54:59 -0800

    Added Bot class with Basic AI support

--- src/logic.py
+++ src/logic.py
@@ -91,108 +91,48 @@ class Board:
 class Human:
 
     def __init__(self, player):
-        """Initializes the player with O or X"""
+        """Initializes the player with O or X."""
         self.player = player
+        self.next_move = 1
 
-    def get_move(self, board, index):
-        """Check whether the move is valid and update the board if true.
-        Returns True if update successful, False if move is not valid."""
+    def get_next_move(self, board):
+        """Gets the next mode from player and return"""
 
-        if board.check_index(index) is False:
-            return False
-        else:
-            board.update_board(index, self.player)
-        return True
+        print('Player ' + self.player + ' enter the location for your move (1-9): ', end='')
+        self.next_move = input()
 
+        while str.isnumeric(self.next_move) is False or board.check_index(int(self.next_move)) is False:
+            print('Error! Input Invalid! Please Try Again!')
+            print('Player ' + self.player + ' enter the location for your move (1-9): ', end='')
+            self.next_move = input()
 
-# TODO: Delete everything below
-def make_empty_board():
-    return [
-        ['1', '2', '3'],
-        ['4', '5', '6'],
-        ['7', '8', '9']]
+        self.next_move = int(self.next_move)
+        return self.next_move
 
+    def get_move(self, board):
+        """Gets the next best move and updates the board."""
 
-def get_winner(board):
-    """Determines the winner of the given board.
-    Returns 'X', 'O', or None."""
-
-    # Check winner in rows
-    for row in board:
-        if row[0] == row[1] == row[2]:
-            return row[0]
-
-    col = 0
-
-    # Check winner in columns
-    while col < 3:
-        if board[0][col] == board[1][col] == board[2][col]:
-            return board[0][col]
-        col += 1
-
-    # Check winner in two diagonals
-    if board[0][0] == board[1][1] == board[2][2]:
-        return board[0][0]
-
-    if board[0][2] == board[1][1] == board[2][0]:
-        return board[0][2]
-
-    return None
+        self.next_move = self.get_next_move(board)
+        board.update_board(self.next_move, self.player)
 
 
-def other_player(player):
-    """Given the character for a player, returns the other player."""
+class Bot:
+    def __init__(self, player):
+        """Initializes the player with O or X."""
+        self.player = player
+        self.next_move = 1
 
-    if player == 'O':
-        return 'X'
+    def get_next_move(self, board):
+        """Bot logic for compute next best move.
+        Returns the index of the next move."""
 
-    return 'O'
+        for i in range(1, 9):
+            if board.check_index(i) is True:
+                print('Player ' + self.player + ' plays at position (1-9): ' + str(i))
+                return i
 
+    def get_move(self, board):
+        """Gets the next best move and updates the board."""
 
-def get_board_from_index(board, index):
-    """Given the board and the index, returns the character on that board."""
-
-    row = int((index - 1) / 3)
-    col = int((index - 1) % 3)
-
-    return board[row][col]
-
-
-def check_index(board, index):
-    """Given the board and the index, check if the index from the player is valid or not."""
-
-    if index < 1 or index > 9:
-        return False
-    if get_board_from_index(board, index) == 'X' or get_board_from_index(board, index) == 'O':
-        return False
-
-    return True
-
-
-def update_board(board, index, player):
-    """Given the board, the index, and the player, update and returns the board for the player."""
-
-    if not check_index(board, index):
-        return board
-
-    row = int((index - 1) / 3)
-    col = int((index - 1) % 3)
-
-    board[row][col] = player
-
-    return board
-
-
-def check_draw(board):
-    """Given the board, check if the board is full and no winner can be determined.
-    Returns True or False"""
-
-    if get_winner(board) is not None:
-        return False
-
-    for row in board:
-        for col in row:
-            if col != 'O' and col != 'X':
-                return False
-
-    return True
+        self.next_move = self.get_next_move(board)
+        board.update_board(self.next_move, self.player)
